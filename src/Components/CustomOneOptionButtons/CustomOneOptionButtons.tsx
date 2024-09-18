1510f4369f22df914702bbd4efe622043639e866
andreasZel <antreaszel@gmail.com>
2024-09-18 23:07:31 +0300

    created new custom one option buttons component for reusability

---
+++ src/Components/CustomOneOptionButtons/CustomOneOptionButtons.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import './CustomOneOptionButtons.css';
+
+export const CustomOneOptionButtons = ({ buttons, initialFocus = -1 }: ButtonProps) => {
+
+    const [focusedBtn, setFocusedBtn] = useState<number>(initialFocus);
+
+    return (
+        <div className="customOneOptionButtonsWrapper">
+            {
+                buttons?.map((button, index) => {
+                    return <button
+                        key={index}
+                        style={{ opacity: (focusedBtn != index || focusedBtn == null) ? '0.6' : '1' }}
+                        className={buttons?.length == 2 ? 'twoOptionButton' : 'multipleOptionButton'}
+                        onClick={() => { button.onClickCallBack(); setFocusedBtn(index) }}>
+                        {!button?.onlyIcon ? button?.title : <img src={button?.icon?.src} width={button?.icon?.width} />}
+                    </button>
+                })
+            }
+        </div>
+    )
+}
+
+type ButtonProps = {
+    buttons: Button[];
+    initialFocus?: number;
+}
+
+
+type Button = {
+    onlyIcon: boolean;
+    title?: string;
+    icon?: { src: string, width: number };
+    onClickCallBack: () => any
+}
