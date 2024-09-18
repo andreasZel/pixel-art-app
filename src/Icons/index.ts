c59d8c6155f6361d18095b0bd84406a3c1722a7b
andreasZel <antreaszel@gmail.com>
2024-09-18 23:03:58 +0300

    added new icons for drawing modes

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,2 +1,4 @@
 export const ShopIcon: string = require('./Shop.png');
 export const PencilIcon: string = require('./Pencil.png');
+export const ClickDrawIcon: string = require('./ClickDraw.png');
+export const FreeDrawIcon: string = require('./FreeDraw.png');
