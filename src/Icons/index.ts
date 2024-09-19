a6461a66c7a4a6da7c74566f1a2c013147b6682d
andreasZel <antreaszel@gmail.com>
2024-09-19 21:55:24 +0300

    added new option icons

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -2,3 +2,6 @@ export const ShopIcon: string = require('./Shop.png');
 export const PencilIcon: string = require('./Pencil.png');
 export const ClickDrawIcon: string = require('./ClickDraw.png');
 export const FreeDrawIcon: string = require('./FreeDraw.png');
+export const ConvertIcon: string = require('./Convert.png');
+export const AddImageIcon: string = require('./AddImage.png');
+export const PixelDrawingIcon: string = require('./PixelDrawing.png');
