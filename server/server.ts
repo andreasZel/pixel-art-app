2e727ef00a96c51b96c63831e3844265e5bdbb6c
andreasZel <antreaszel@gmail.com>
2024-09-22 23:27:48 +0300

    added pg and updated start script

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,5 @@
+import drawingsRouter from './api/router/drawingsRouter';
+
 const express = require('express');
 
 const app = express();
@@ -5,6 +7,8 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+app.use('/drawnings', drawingsRouter())
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
