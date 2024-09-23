efebaf0d87bd1716d7e0538ac49e581d784495c0
andreasZel <antreaszel@gmail.com>
2024-09-23 21:10:05 +0300

    fixed get all drawings and added create drawing method

--- server/api/router/drawingsRouter.ts
+++ server/api/router/drawingsRouter.ts
@@ -1,17 +1,36 @@
-import { Router } from "express";
+import { json, Router } from "express";
 import pool from "../../database";
 
 export default function drawingsRouter() {
     const app = Router();
 
-    app.get("/all", async (req, res) => {
+    app.get("/all/:ownerid", async (req, res) => {
+        if (!req?.params?.ownerid)
+            res.status(400).send("/all/:ownerid, Ownerid missing");
+
         try {
-            const result = await pool.query('SELECT * FROM drawings');
-            console.log(result)
+            const result = await pool.query(`SELECT * FROM drawings WHERE id=${req.params.ownerid}`);
             res.json(result.rows);
         } catch (e) {
-            console.log("Something went wrong drawingsRouter():\n" + e);
-            res.status(500).send("Something went wrong");
+            console.log("Something went wrong /all/:ownerid => drawingsRouter():\n" + e);
+            res.status(500).send("Something went wrong in /all/:ownerid => drawingsRouter()");
+        }
+    });
+
+    app.post("/saveDrawing/:ownerid", json(), async (req, res) => {
+        if (!req?.params?.ownerid)
+            res.status(400).send("/all/:ownerid, Ownerid missing");
+
+        const { pixelids, pixelcolors } = req.body
+
+        try {
+            const query = "INSERT INTO drawings (pixelids, pixelcolors, ownerid) VALUES ($1, %2, $3) RETURNING *";
+
+            const queryResult = await pool.query(query, [pixelids, pixelcolors, req.params.ownerid]);
+            res.json(queryResult.rows);
+        } catch (e) {
+            console.log("Something went wrong /saveDrawing/:ownerid => drawingsRouter():\n" + e);
+            res.status(500).send("Something went wrong in /saveDrawing/:ownerid => drawingsRouter()");
         }
     });
 
