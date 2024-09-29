297449ddcb4fb87905f380fae0a557da1207a2e7
andreasZel <antreaszel@gmail.com>
2024-09-29 16:51:32 +0300

    fixed server and router endpoints and host outside containerized app

--- server/database.ts
+++ server/database.ts
@@ -1,8 +1,8 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    host: process.env.POSTGRES_HOST || 'http://localhost',
-    user: process.env.POSTGRES_USER || 'postgress',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    user: process.env.POSTGRES_USER || 'postgres',
     password: process.env.POSTGRES_PASSWORD || 'postgress',
     database: process.env.POSTGRES_DB || 'postgres',
     port: process.env.POSTGRES_PORT as number | undefined || 8080,
