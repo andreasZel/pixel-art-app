b3274a51fe987263b85ef3abecf2401f5799550a
andreasZel <antreaszel@gmail.com>
2024-09-29 16:01:41 +0300

    altered variables for non containerized application

--- server/database.ts
+++ server/database.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT as number | undefined,
+    host: process.env.POSTGRES_HOST || 'http://localhost',
+    user: process.env.POSTGRES_USER || 'postgress',
+    password: process.env.POSTGRES_PASSWORD || 'postgress',
+    database: process.env.POSTGRES_DB || 'postgres',
+    port: process.env.POSTGRES_PORT as number | undefined || 8080,
     idleTimeoutMillis: 30000,
 });
 
