3e4271ec71555e6b18713a2f8fea5b691fd9d80a
andreasZel <antreaszel@gmail.com>
2024-10-29 21:19:15 +0200

    created login function and removed unnecessary change functions

--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -1,20 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import { DrawingAnimation } from "../DrawingAnimation/DrawingAnimation";
 import { StyledButton } from "../StyledButton/StyledButton";
 import './LoginPage.css';
 import React, { useRef } from "react";
 
 export const LoginPage = () => {
+    let navigate = useNavigate();
 
-    const Username = useRef(null);
-    const Password = useRef(null);
+    const Username = useRef<HTMLInputElement | null>(null);
+    const Password = useRef<HTMLInputElement | null>(null);
 
-    const login = () => {
+    const login = async () => {
 
-    }
+        if (Username?.current?.value && Password?.current?.value) {
+            const res = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: Username?.current?.value,
+                    password: Password?.current?.value
+                })
+            });
 
-    const alterValue = (input: React.MutableRefObject<any>, e: React.ChangeEvent<HTMLInputElement>) => {
-        if (input.current) {
-            input.current.value = e.target.value
+            if (res.status == 200) {
+                navigate('/');
+            }
+
         }
     }
 
@@ -27,13 +40,13 @@ export const LoginPage = () => {
             <div className="loginInputsArea">
                 <div className="loginInputWrapper" style={{ marginTop: '40px' }}>
                     <label>Username:</label>
-                    <input ref={Username} onChange={(e) => { alterValue(Username, e) }} />
+                    <input type="string" ref={Username} />
                 </div>
                 <div className="loginInputWrapper">
                     <label>Password:</label>
-                    <input ref={Password} onChange={(e) => { alterValue(Password, e) }} />
+                    <input type="string" ref={Password} />
                 </div>
-                <StyledButton title={'Login'} style={{ position: 'absolute', bottom: '15px', right: '25px', width: '200px', height: '30', fontSize: '40px', backgroundColor: 'var(--generic-button-color)', padding: '10px 20px' }} onClickCallBack={login} />
+                <StyledButton title={'Login'} style={{ position: 'absolute', bottom: '15px', right: '25px', width: '200px', height: '30', fontSize: '40px', backgroundColor: 'var(--generic-button-color)', padding: '10px 20px' }} onClickCallBack={() => { login() }} />
             </div>
         </div>
     </>
