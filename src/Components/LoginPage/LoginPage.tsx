acbc73ba485f68479d6ef7e4debbb49f1230ad0f
andreasZel <antreaszel@gmail.com>
2024-10-29 20:41:49 +0200

    added ref to both inputs

--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -1,12 +1,21 @@
 import { DrawingAnimation } from "../DrawingAnimation/DrawingAnimation";
 import { StyledButton } from "../StyledButton/StyledButton";
 import './LoginPage.css';
-import React from "react";
+import React, { useRef } from "react";
 
 export const LoginPage = () => {
 
+    const Username = useRef(null);
+    const Password = useRef(null);
+
     const login = () => {
 
+    }
+
+    const alterValue = (input: React.MutableRefObject<any>, e: React.ChangeEvent<HTMLInputElement>) => {
+        if (input.current) {
+            input.current.value = e.target.value
+        }
     }
 
     return (<>
@@ -14,15 +23,15 @@ export const LoginPage = () => {
             <DrawingAnimation />
         </div>
         <div className="loginWrapper">
-            <label>Pixel Art Application</label>
+            <label style={{ filter: 'drop-shadow(2px 4px 6px black)' }}>Pixel Art Application</label>
             <div className="loginInputsArea">
                 <div className="loginInputWrapper" style={{ marginTop: '40px' }}>
                     <label>Username:</label>
-                    <input />
+                    <input ref={Username} onChange={(e) => { alterValue(Username, e) }} />
                 </div>
                 <div className="loginInputWrapper">
                     <label>Password:</label>
-                    <input />
+                    <input ref={Password} onChange={(e) => { alterValue(Password, e) }} />
                 </div>
                 <StyledButton title={'Login'} style={{ position: 'absolute', bottom: '15px', right: '25px', width: '200px', height: '30', fontSize: '40px', backgroundColor: 'var(--generic-button-color)', padding: '10px 20px' }} onClickCallBack={login} />
             </div>
